77b8f9bba47f823bf834cb65bdb0b1f9f774ffcc
Igor-JCI <7.jciab.7@gmail.com>
2022-08-02 20:05:47 +0300

    01-Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://img2.freepng.ru/20180219/vte/kisspng-logo-blue-blue-triangle-irregular-graphics-5a8b9aebce7332.2835631215190986038456.jpg"/>
+        </header>
+    )
+}
+
+
+export default Header
