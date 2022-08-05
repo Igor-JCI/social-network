97ef448fbedfd1b512577b94d1f2781cba8851c2
Igor-JCI <7.jciab.7@gmail.com>
2022-08-05 09:44:13 +0300

    02-Commit-CSS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-
+import s from "./Header.module.css"
 const Header = () => {
     return (
-        <header className="header">
-            <img
-                src="https://img2.freepng.ru/20180219/vte/kisspng-logo-blue-blue-triangle-irregular-graphics-5a8b9aebce7332.2835631215190986038456.jpg"/>
+        <header className={s.header}>
+            <img src="https://img2.freepng.ru/20180219/vte/kisspng-logo-blue-blue-triangle-irregular-graphics-5a8b9aebce7332.2835631215190986038456.jpg"/>
         </header>
     )
 }
