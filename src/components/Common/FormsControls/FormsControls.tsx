0369b9a33f06de5653abb6080a003381eb20f946
Igor-JCI <7.jciab.7@gmail.com>
2023-01-15 16:12:06 +0500

    59-87 - shouldComponentUpdate, PureComponent,memo refactoring 3

--- src/components/Common/FormsControls/FormsControls.tsx
+++ src/components/Common/FormsControls/FormsControls.tsx
@@ -1,4 +1,4 @@
-import React, {FC, HTMLInputTypeAttribute, ReactNode} from "react";
+import React, {FC, HTMLInputTypeAttribute} from "react";
 import {WrappedFieldInputProps, WrappedFieldMetaProps} from "redux-form";
 import styles from "./FormsControls.module.css"
 
@@ -24,41 +24,9 @@ export const Textarea: FC<FormsControls> = (props) => {
     const {input, ...restProps} = props
     return <FormControl{...props}><textarea {...input} {...restProps}/></FormControl>
 }
-
 export const Input: FC<FormsControls> = (props) => {
     const {input, ...restProps} = props
     return <FormControl{...props}><input {...input} {...restProps}/></FormControl>
 }
 
 
-
-/*
-const FormControl: FC<FormsControls> = ({input, meta, child, ...props}) => {
-    const hasError = meta.touched && meta.error
-    return <div className={hasError ? styles.error :""}>
-        <div>
-            <textarea {...input} {...props}/>
-        </div>
-        {hasError && <span>{meta.error}</span>}
-    </div>
-}
-export const Textarea: FC<FormsControls> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error
-    return <div className={hasError ? styles.error :""}>
-        <div>
-            <textarea {...input} {...props}/>
-        </div>
-        {hasError && <span>{meta.error}</span>}
-    </div>
-}
-
-export const Input: FC<FormsControls> = ({input, meta, ...props}) => {
-    const hasError = meta.touched && meta.error
-    return <div className={hasError ? styles.error :""}>
-        <div>
-            <input {...input} {...props}/>
-        </div>
-        {hasError && <span>{meta.error}</span>}
-    </div>
-}
-*/
