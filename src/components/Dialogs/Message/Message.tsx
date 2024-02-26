b37dd1384c284847aff45c2757492edad6245d2d
Igor-JCI <7.jciab.7@gmail.com>
2024-02-26 20:10:28 +0300

    70-103 - refactoring 4.0

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -1,12 +1,9 @@
 import React from "react";
 import s from "./../Dialogs.module.css"
 
-
 type MessagePropsType = {
     message:string
 }
-
-
 export const Message = (props:MessagePropsType) => {
 
     return(
